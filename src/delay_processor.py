0609d6a936c4c1cdbb23747450e7233fe556c806
Mohamed Aslam <asluuu@gmail.com>
2026-06-21 19:41:21 +0530

    Fix memory delayed tracking

---
+++ src/delay_processor.py
@@ -0,0 +1,11 @@
+Every 15 mins:
+    find pending conversations
+    check age > 15 mins
+    ai_replied = false
+    manual_replied = false
+
+    generate reply
+    send reply
+
+    ai_replied = true
+    pending_reply = false
