295280d157e7a7ae976b984ff610c3ce63149638
asluuu-archon <asluuu@gmail.com>
2026-07-10 07:43:40 +0530

    Fix CORS: allow all origins

--- src/app.py
+++ src/app.py
@@ -39,11 +39,8 @@ from .playground import (
 app = FastAPI(title="SAP Guru Assistant", version="pilot_3")
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
-    ],
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
